89a830ce85886bb6255b3832d5ee81771b312a3c
Ghizmilth <hidanerferrer@gmail.com>
2017-07-11 10:30:18 -0700

    Added count to the numb of times key was pressed.js

--- main.js
+++ main.js
@@ -2,12 +2,15 @@
 $(document).ready(function () {
 console.log('You can code NOW!')
 
+var moveBox = 0;
 
 //Detects which key has been pressed so it moves player
 $(document).keydown(function(press) {
     if (press.keyCode === 39) {
-      //move(keyCode);
-      console.log('right arrow has been pressed');
+    console.log('right arrow has been pressed times');
+    moveBox += 1;
+    moveCar(moveBox);
+    console.log(moveBox);
     } else {
       return;
     }
@@ -20,7 +23,20 @@ $(document).keydown(function(press) {
 
 
 //This function moves the player from left to right
-//function move() {
-  //var square = $('#slot1')
-  //for (var i = 0; i < 24; i++)
-//}
+function moveCar(moveBox) {
+  $()
+
+  //  if (moveBox) {
+      //var boxPosition = 0;
+      //boxPosition += 1;
+      console.log("box number " + moveBox  + " " + "has been ocupied");
+    } else {
+      console.log("can't move any more");
+    }
+
+//  } else
+    //return;
+  //  console.log("can't move no more");
+}
+
+};
